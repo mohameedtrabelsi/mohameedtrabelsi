9eba3dcae6880d32d9389d2ee5e78955ec454dbc
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-01-24 17:36:33 -0500

    Update agentinstall.ps1

--- agentinstall.ps1
+++ agentinstall.ps1
@@ -1,3 +1,49 @@
+############################################
+#####GIT INSTALL############################
+############################################
+# get latest download url for git-for-windows 64-bit exe
+$git_url = "https://api.github.com/repos/git-for-windows/git/releases/latest"
+$asset = Invoke-RestMethod -Method Get -Uri $git_url | % assets | where name -like "*64-bit.exe"
+# download installer
+$installer = "$env:temp\$($asset.name)"
+Invoke-WebRequest -Uri $asset.browser_download_url -OutFile $installer
+# run installer
+$git_install_inf = "<install inf file>"
+$install_args = "/SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS /LOADINF=""$git_install_inf"""
+Start-Process -FilePath $installer -ArgumentList $install_args -Wait
+
+#####################################################################################
+#####Disable IE security on Windows Server via PowerShell############################
+#####################################################################################
+
+function Disable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0 -Force
+    Stop-Process -Name Explorer -Force
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+function Enable-InternetExplorerESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 1 -Force
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 1 -Force
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been enabled." -ForegroundColor Green
+}
+function Disable-UserAccessControl {
+    Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 00000000 -Force
+    Write-Host "User Access Control (UAC) has been disabled." -ForegroundColor Green    
+}
+Disable-UserAccessControl
+Disable-InternetExplorerESC
+
+
+############################################
+#####AGENT INSTALL############################
+############################################
+
 param (
     [Parameter(Mandatory)][string]$URL,
     [Parameter(Mandatory)][string]$PAT,
