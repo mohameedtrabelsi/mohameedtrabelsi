44b2a265139522f8344edf39198f4b3a933fd78a
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-01-23 19:59:03 -0500

    Update installAZCLIetPSHL.ps1

--- installAZCLIetPSHL.ps1
+++ installAZCLIetPSHL.ps1
@@ -11,12 +11,12 @@ $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/
 
 #Etape1 : Verify the .NET Framework 4.7.2 or higher is installed
 
-(Get-ItemProperty -Path 'HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Full' -ErrorAction SilentlyContinue).Release -ge 461808
+#(Get-ItemProperty -Path 'HKLM:\Software\Microsoft\NET Framework Setup\NDP\v4\Full' -ErrorAction SilentlyContinue).Release -ge 461808
 
 #Etape 2 : PowerShell script execution policy must be set to remote signed
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
 #Etape : Installation Module AZ
-Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
+#Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 
 
