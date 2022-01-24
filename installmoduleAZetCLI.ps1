4c600114073367eeffb9c2186a429ff92be6e89f
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-01-24 11:10:55 -0500

    Add files via upload

---
+++ installmoduleAZetCLI.ps1
@@ -0,0 +1,19 @@
+﻿###################################
+###Install Azure CLI on Windows###
+##################################
+
+$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+
+
+#############################################
+###Install the Azure Az PowerShell module###
+############################################
+
+#Etape : Installation Module AZ
+install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+#Etape : register the default repository for PowerShell modules
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+#Etape : Install the module AZ
+Install-Module -Name Az
