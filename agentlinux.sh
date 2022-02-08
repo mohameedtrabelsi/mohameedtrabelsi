6bd6ba77371af2aaa9feb13df31da715d4ac95aa
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-08 18:20:42 -0500

    Update agentlinux.sh

--- agentlinux.sh
+++ agentlinux.sh
@@ -10,8 +10,8 @@ tar zxvf vsts-agent-linux-x64-2.196.2.tar.gz
 --agent "${AZP_AGENT_NAME:-$(hostname)}" \
 --url "https://dev.azure.com/allymeer-hossen/" \
 --auth PAT \
---token "<wzjlas6igiiitg7yrh4fgyehgfeokdy4nyoaqab54xlo32qlgt2a>" \
---pool "testlinux" \
+--token "lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq" \
+--pool "testing" \
 --replace \
 --acceptTeeEula & wait $!
 
