574aad95ba5f5a119c6d5221e6ae7862e3bc5020
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-08 16:32:15 -0500

    Update testdebian.sh

--- testdebian.sh
+++ testdebian.sh
@@ -3,9 +3,6 @@ pool="testing"
 pat="lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq"
 azdourl="https://dev.azure.com/allymeer-hossen/"
 
-# install az cli
-wget https://aka.ms/InstallAzureCLIDeb | sudo bash
-
 # download azdo agent
 sudo mkdir $(pwd)/myagent
 cd $(pwd)/myagent
