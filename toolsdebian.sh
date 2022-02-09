fc8d58c9a293389412c3ed4b3b534076eea29f88
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-09 11:34:27 -0500

    Update toolsdebian.sh

--- toolsdebian.sh
+++ toolsdebian.sh
@@ -1,8 +1,11 @@
 #GIT
-apt install build-essential make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
+sudo apt update && sudo apt upgrade
+sudo apt install curl
+sudo apt install python3-pycurl
+sudo apt install build-essential make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
 cd /tmp/
-wget wget https://github.com/git/git/archive/v2.21.0.zip -O latestgit.zip
-unzip latestgit.zip
+sudo wget wget https://github.com/git/git/archive/v2.21.0.zip -O latestgit.zip
+unzip latestgit.zip 
 cd git-2.21.0
 sudo make prefix=/usr/local all
 sudo make prefix=/usr/local install
@@ -14,6 +17,6 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt update
 sudo apt install powershell
 #Terraform
-#sudo wget https://releases.hashicorp.com/terraform/1.1.5/terraform_1.1.5_linux_amd64.zip
-#sudo apt install unzip && unzip terraform_1.1.5_linux_amd64.zip
-#sudo mv terraform /usr/local/bin/
+sudo wget https://releases.hashicorp.com/terraform/1.1.5/terraform_1.1.5_linux_amd64.zip
+sudo apt install unzip && unzip terraform_1.1.5_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
