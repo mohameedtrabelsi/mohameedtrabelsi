592cbf0ca72b3b399ce222a82c8c2bd1573ae094
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-09 15:30:10 -0500

    Update testdebian.sh

--- testdebian.sh
+++ testdebian.sh
@@ -1,17 +1,27 @@
-su - azureuser -c "
-mkdir $(pwd)/myagent
-cd $(pwd)/myagent
-wget https://vstsagentpackage.azureedge.net/agent/2.196.2/vsts-agent-linux-x64-2.196.2.tar.gz # Newer versions may be available at the time you're reading this
-tar xzvf *.tar.gz
-echo pwd >> /tmp/echofile
-# configure as azdouser
-cd ..
-chmod -R o+w $(pwd)/myagent
-cd $(pwd)/myagent
-./config.sh azureuser --unattended --url "https://dev.azure.com/allymeer-hossen/" --auth pat --token "lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq" --pool "testing" --agent $(hostname) --runAsService
+#!/bin/sh
 
-# install and start the service
-./run.sh
-#sudo ./svc.sh install azureuser
-#sudo ./svc.sh start
-#sudo ./svc.sh status
+# Creates directory & download ADO agent install files
+
+su - azureuser -c "
+mkdir myagent && cd myagent
+wget https://vstsagentpackage.azureedge.net/agent/2.186.1/vsts-agent-linux-x64-2.186.1.tar.gz
+tar zxvf vsts-agent-linux-x64-2.186.1.tar.gz"
+
+# Unattended install
+
+su - azureuser -c "
+./config.sh --unattended \
+  --agent "${AZP_AGENT_NAME:-$(hostname)}" \
+  --url "https://dev.azure.com/allymeer-hossen" \
+  --auth PAT \
+  --token "lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq" \
+  --pool "testing" \
+  --replace \
+  --acceptTeeEula & wait $!"
+
+cd /home/azureuser/
+#Configure as a service
+sudo ./svc.sh install azureuser
+
+#Start svc
+sudo ./svc.sh start
