0b13e1cfc3255c8874866ca2f389f25977362d66
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-09 16:15:01 -0500

    Update testdebian.sh

--- testdebian.sh
+++ testdebian.sh
@@ -7,7 +7,7 @@ tar zxvf *.tar.gz
 # Unattended install
 su - azureuser -c "
 cd /home/azureuser/myagent
-./config.sh--unattended --url "https://dev.azure.com/allymeer-hossen/" --auth pat --token "lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq" --pool "testing" --agent $(hostname) --runAsService
+./config.sh --unattended --url "https://dev.azure.com/allymeer-hossen/" --auth pat --token "lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq" --pool "testing" --agent $(hostname) --runAsService
 #./config.sh --unattended --agent "${AZP_AGENT_NAME:-$(hostname)}" --url "https://dev.azure.com/allymeer-hossen" --auth PAT \
 # # --token "lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq" \
   #--pool "testing" \
