af7a168fdf6a0b2c2d23f1df03c070fc662827f5
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-09 15:15:11 -0500

    Update testdebian.sh

--- testdebian.sh
+++ testdebian.sh
@@ -1,16 +1,15 @@
-# download azdo agent
 mkdir $(pwd)/myagent
 cd $(pwd)/myagent
 wget https://vstsagentpackage.azureedge.net/agent/2.196.2/vsts-agent-linux-x64-2.196.2.tar.gz # Newer versions may be available at the time you're reading this
 tar xzvf *.tar.gz
+
 # configure as azdouser
 cd ..
 chmod -R o+w $(pwd)/myagent
 cd $(pwd)/myagent
-./config.sh --unattended --url "https://dev.azure.com/allymeer-hossen/" --auth pat --token "lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq" --pool "testing" --agent $(hostname) --runAsService
+./config.sh azureuser --unattended --url "https://dev.azure.com/allymeer-hossen/" --auth pat --token "lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq" --pool "testing" --agent $(hostname) --runAsService
 
 # install and start the service
-./run.sh
-#sudo ./svc.sh install
-#sudo ./svc.sh start
-#sudo ./svc.sh status
+sudo ./svc.sh install azureuser
+sudo ./svc.sh start
+sudo ./svc.sh status
