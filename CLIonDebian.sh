332c4fb1311cee25fbc48aac957f36f0caaead15
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-08 19:14:05 -0500

    Update CLIonDebian.sh

--- CLIonDebian.sh
+++ CLIonDebian.sh
@@ -1,6 +1,10 @@
 # install az cli
 wget https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] <https://packages.microsoft.com/repos/azure-cli/> $AZ_REPO main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
 sudo apt-get update
 sudo apt-get install azure-cli
 
