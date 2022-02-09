aebffd516dae76e15bf714046510b67c13c1b52d
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-08 21:01:06 -0500

    Update terraformlinux.sh

--- terraformlinux.sh
+++ terraformlinux.sh
@@ -2,11 +2,11 @@ wget https://releases.hashicorp.com/terraform/1.1.4/terraform_1.1.4_linux_amd64.
 sudo apt install unzip && unzip terraform_1.1.4_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
-mkdir /opt/terraform
-cd /opt/terraform
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt install terraform
+#mkdir /opt/terraform
+#cd /opt/terraform
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+#sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+#sudo apt install terraform
 
 
 
