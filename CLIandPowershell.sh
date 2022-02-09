070268dcf2c5b7308e904f7b541f0527a4e3543c
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-08 20:09:10 -0500

    Update CLIandPowershell.sh

--- CLIandPowershell.sh
+++ CLIandPowershell.sh
@@ -1,3 +1,23 @@
-#Install CLI on Linux 
+#########################
+##Install CLI on Linux###
+#########################
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
+###################
+##PSHELL INSTALL###
+###################
+
+# Download the Microsoft repository GPG keys
+wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb
+
+# Register the Microsoft repository GPG keys
+sudo dpkg -i packages-microsoft-prod.deb
+
+# Update the list of products
+sudo apt-get update
+
+# Install PowerShell
+sudo apt-get install -y powershell
+
+# Start PowerShell
+pwsh
