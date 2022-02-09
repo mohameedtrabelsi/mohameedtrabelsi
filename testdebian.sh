45e83f5cd90f592ca856ad133c5dbc5728a468ef
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-09 16:00:48 -0500

    Update testdebian.sh

--- testdebian.sh
+++ testdebian.sh
@@ -3,7 +3,7 @@
 su - azureuser -c "
 mkdir myagent && cd myagent
 wget https://vstsagentpackage.azureedge.net/agent/2.196.2/vsts-agent-linux-x64-2.196.2.tar.gz
-tar zxvf vsts-agent-linux-x64-2.196.2.tar.gz
+tar zxvf *.tar.gz
 # Unattended install
 su - azureuser -c "
 ./config.sh --unattended --url "https://dev.azure.com/allymeer-hossen/" --auth pat --token "lukspdn2imzgatakygjlix4ecypl76z67gytwzf4hd3ush6i24wq" --pool "testing" --agent $(hostname) --runAsService
