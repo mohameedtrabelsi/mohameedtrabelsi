c0eceed6cdf633e67cf9c0a5e2571223d5e2147d
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-10 12:05:04 -0500

    Update alltoolslinux.sh

--- alltoolslinux.sh
+++ alltoolslinux.sh
@@ -2,6 +2,8 @@
 sudo wget https://releases.hashicorp.com/terraform/1.1.5/terraform_1.1.5_linux_amd64.zip
 sudo apt install unzip && unzip terraform_1.1.5_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
+##Install CLI on Linux###
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 #agentdevops
 sudo mkdir /myagent
 cd /myagent
