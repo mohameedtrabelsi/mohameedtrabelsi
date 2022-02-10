043e5ba6daf78dbca08240012a87f25f56e89f8b
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-10 14:41:06 -0500

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -6,7 +6,7 @@ cd /myagent
 sudo wget https://vstsagentpackage.azureedge.net/agent/2.186.1/vsts-agent-linux-x64-2.186.1.tar.gz
 sudo tar zxvf ./vsts-agent-linux-x64-2.186.1.tar.gz
 sudo chmod -R 777 /myagent
-runuser -l azureuser -c '/myagent/config.sh --unattended  --url $1 --auth pat --token $2 --pool $3'
+runuser -l azureuser -c "/myagent/config.sh --unattended  --url $1 --auth pat --token $2 --pool $3"
 sudo /myagent/svc.sh install
 sudo /myagent/svc.sh start
 exit 0
