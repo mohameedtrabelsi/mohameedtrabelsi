71bb7f833f8ca35bb85c271120e0a3ba44d81592
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-10 09:15:08 -0500

    Delete terraformlinux.sh

--- terraformlinux.sh
+++
@@ -1,9 +0,0 @@
-wget https://releases.hashicorp.com/terraform/1.1.5/terraform_1.1.5_linux_amd64.zip
-sudo apt install unzip && unzip terraform_1.1.5_linux_amd64.zip
-sudo mv terraform /usr/local/bin/
-
-
-
-#$ which terraform
-#/usr/local/bin/terraform
-#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
