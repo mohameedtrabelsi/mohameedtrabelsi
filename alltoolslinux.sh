f98cd75066b23017b05b92ccfc81c948d9727cdc
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-10 12:27:07 -0500

    Update alltoolslinux.sh

--- alltoolslinux.sh
+++ alltoolslinux.sh
@@ -2,8 +2,7 @@
 sudo wget https://releases.hashicorp.com/terraform/1.1.5/terraform_1.1.5_linux_amd64.zip
 sudo apt install unzip && unzip terraform_1.1.5_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
-##Install CLI on Linux###
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
 #agentdevops
 sudo mkdir /myagent
 cd /myagent
@@ -15,16 +14,17 @@ sudo /myagent/svc.sh install
 sudo /myagent/svc.sh start
 
 #GIT
-#sudo apt update && sudo apt upgrade
-#sudo apt install curl
-#sudo apt install python3-pycurl
-#sudo apt install build-essential make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
-#cd /tmp/
-#sudo wget https://github.com/git/git/archive/v2.21.0.zip -O latestgit.zip
-#unzip latestgit.zip 
-#cd git-2.21.0
-#sudo make prefix=/usr/local all
-#sudo make prefix=/usr/local install
+cd ..
+sudo apt update && sudo apt upgrade
+sudo apt install curl
+sudo apt install python3-pycurl
+sudo apt install build-essential make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
+cd /tmp/
+sudo wget https://github.com/git/git/archive/v2.21.0.zip -O latestgit.zip
+unzip latestgit.zip 
+cd git-2.21.0
+sudo make prefix=/usr/local all
+sudo make prefix=/usr/local install
 
 ##Install CLI on Linux###
 #curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
