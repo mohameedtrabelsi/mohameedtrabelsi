94725a3fbb9de6c4ab026205cacd6c4fbd15e018
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-10 11:57:52 -0500

    Update alltoolslinux.sh

--- alltoolslinux.sh
+++ alltoolslinux.sh
@@ -1,3 +1,7 @@
+#Terraform
+sudo wget https://releases.hashicorp.com/terraform/1.1.5/terraform_1.1.5_linux_amd64.zip
+sudo apt install unzip && unzip terraform_1.1.5_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
 #agentdevops
 sudo mkdir /myagent
 cd /myagent
@@ -9,26 +13,23 @@ sudo /myagent/svc.sh install
 sudo /myagent/svc.sh start
 
 #GIT
-sudo apt update && sudo apt upgrade
-sudo apt install curl
-sudo apt install python3-pycurl
-sudo apt install build-essential make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
-cd /tmp/
-sudo wget https://github.com/git/git/archive/v2.21.0.zip -O latestgit.zip
-unzip latestgit.zip 
-cd git-2.21.0
-sudo make prefix=/usr/local all
-sudo make prefix=/usr/local install
+#sudo apt update && sudo apt upgrade
+#sudo apt install curl
+#sudo apt install python3-pycurl
+#sudo apt install build-essential make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
+#cd /tmp/
+#sudo wget https://github.com/git/git/archive/v2.21.0.zip -O latestgit.zip
+#unzip latestgit.zip 
+#cd git-2.21.0
+#sudo make prefix=/usr/local all
+#sudo make prefix=/usr/local install
 
 ##Install CLI on Linux###
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 #PSHELL
-sudo wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-sudo apt update
-sudo apt install powershell
+#sudo wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb
+#sudo dpkg -i packages-microsoft-prod.deb
+#sudo apt update
+#sudo apt install powershell
 
-#Terraform
-#sudo wget https://releases.hashicorp.com/terraform/1.1.5/terraform_1.1.5_linux_amd64.zip
-#sudo apt install unzip && unzip terraform_1.1.5_linux_amd64.zip
-#sudo mv terraform /usr/local/bin/
+
