704123438deb6fa9bd4d24fbd93e3a7b9befe3c8
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-11 17:20:05 -0500

    Update toolsforlinux.sh

--- toolsforlinux.sh
+++ toolsforlinux.sh
@@ -3,27 +3,7 @@ sudo wget https://releases.hashicorp.com/terraform/1.1.5/terraform_1.1.5_linux_a
 sudo apt install unzip && unzip terraform_1.1.5_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 #GIT
-sudo apt install build-essential make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
-cd /tmp/
-sudo wget https://github.com/git/git/archive/v2.21.0.zip -O latestgit.zip
-unzip latestgit.zip 
-cd git-2.21.0
-sudo make prefix=/usr/local all
-sudo make prefix=/usr/local install
-#####PSHELL
-# Install system components
-sudo apt-get update
-sudo apt-get install -y curl gnupg apt-transport-https
-# Import the public repository GPG keys
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-# Register the Microsoft Product feed
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/microsoft.list'
-# Update the list of products
-sudo apt-get update
-# Install PowerShell
-sudo apt-get install -y powershell
-#Azure CLI
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo apt install git-all -y
 #Agentdevops
 #echo "$1" > /tmp/echofile
 #echo "$2" > /tmp/echofile2
