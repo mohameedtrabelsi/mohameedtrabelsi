0551fb5c021cb711f289d55763b33742cd24046c
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-11 16:41:01 -0500

    Update toolsforlinux.sh

--- toolsforlinux.sh
+++ toolsforlinux.sh
@@ -10,6 +10,18 @@ unzip latestgit.zip
 cd git-2.21.0
 sudo make prefix=/usr/local all
 sudo make prefix=/usr/local install
+#####PSHELL
+# Install system components
+sudo apt-get update
+sudo apt-get install -y curl gnupg apt-transport-https
+# Import the public repository GPG keys
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+# Register the Microsoft Product feed
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/microsoft.list'
+# Update the list of products
+sudo apt-get update
+# Install PowerShell
+sudo apt-get install -y powershell
 #Agentdevops
 #echo "$1" > /tmp/echofile
 #echo "$2" > /tmp/echofile2
