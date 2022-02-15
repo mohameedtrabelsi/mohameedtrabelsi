4f513a26e0d1e495e37c303c5c4c533bc5550c08
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-15 12:39:07 -0500

    Update toolsforlinux.sh

--- toolsforlinux.sh
+++ toolsforlinux.sh
@@ -19,13 +19,21 @@ sudo /myagent/svc.sh install
 sudo /myagent/svc.sh start
 #exit 0
 ##Install CLI on Linux###
+#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 sudo apt-get update
-sudo apt-get install
-sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+gpg --dearmor |
+sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install azure-cli
 ### update OS & Install PSHELL & Module AZ
 cd /home/azureuser
 sudo apt-get update
-sudo apt-get install -y wget apt-transport-https
+runuser -l azureuser sudo apt-get install -y wget apt-transport-https
 wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
