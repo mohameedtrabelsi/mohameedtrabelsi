927357a00f50c66f1d132320c329d0d08dd4383d
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-16 11:22:08 -0500

    Update toolsforlinux.sh

--- toolsforlinux.sh
+++ toolsforlinux.sh
@@ -39,4 +39,7 @@ sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo add-apt-repository universe
 sudo apt-get install -y powershell
-#pwsh -c Install-Module -Name Az -Force
+pwsh
+pwsh -c "Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force"
+pwsh -c "Install-Module Az -AllowClobber -Force"
+pwsh -c "Update-Module -Name Az -Force"
