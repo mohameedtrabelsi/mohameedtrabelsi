76291f6854a88e779151dbfc4987f8400807c8b2
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-19 22:06:50 -0500

    Create forlinux.sh

---
+++ forlinux.sh
@@ -0,0 +1,61 @@
+#Terraform
+sudo wget https://releases.hashicorp.com/terraform/1.1.5/terraform_1.1.5_linux_amd64.zip
+sudo apt install unzip && unzip terraform_1.1.5_linux_amd64.zip
+sudo mv terraform /usr/local/bin/terraform
+sudo chmod -R 777 /usr/local/bin/terraform
+#### INSTALL AZ Bicep###
+# Fetch the latest Bicep CLI binary
+curl -Lo bicep https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
+# Mark it as executable
+chmod +x ./bicep
+# Add bicep to your PATH (requires admin)
+sudo mv ./bicep /usr/local/bin/bicep
+# Verify you can now access the 'bicep' command
+bicep --help
+# Done!
+#GIT
+sudo apt install git-all -y
+#Agentdevops
+#echo "$1" > /tmp/echofile
+#echo "$2" > /tmp/echofile2
+#echo "$3" > /tmp/echofile3
+sudo mkdir /myagent
+cd /myagent
+sudo wget https://vstsagentpackage.azureedge.net/agent/2.186.1/vsts-agent-linux-x64-2.186.1.tar.gz
+sudo tar zxvf ./vsts-agent-linux-x64-2.186.1.tar.gz
+sudo chmod -R 777 /myagent
+runuser -l azureuser -c "/myagent/config.sh --unattended  --url $1 --auth pat --token $2 --pool $3"
+#/myagent/config.sh --unattended  --url "$1" --auth pat --token "$2" --pool "$3"
+sudo /myagent/svc.sh install
+sudo /myagent/svc.sh start
+#exit 0
+##Install CLI on Linux###
+#curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo apt-get update
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+curl -sL https://packages.microsoft.com/keys/microsoft.asc |
+gpg --dearmor |
+sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
+sudo tee /etc/apt/sources.list.d/azure-cli.list
+sudo apt-get update
+sudo apt-get install azure-cli
+
+### update OS & Install PSHELL & Module AZ
+cd /home/azureuser
+sudo apt-get update
+runuser -l azureuser sudo apt-get install -y wget apt-transport-https
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo add-apt-repository universe
+sudo apt-get install -y powershell
+
+#### INSTALL AZ Module###
+
+wget https://raw.githubusercontent.com/mbouaklaine/MohamedBouaklaine/main/Powershell.sh
+sudo chmod 777 Powershell.sh 
+runuser -l azureuser -c "/home/azureuser/Powershell.sh"
+
+
