d4f5ca9ea674c52b98dba37e7d149cf7aff81cbc
hammatrb <59232361+mohameedtrabelsi@users.noreply.github.com>
2022-02-20 22:25:05 -0500

    Update forlinux.sh

--- forlinux.sh
+++ forlinux.sh
@@ -53,10 +53,10 @@ sudo add-apt-repository universe
 sudo apt-get install -y powershell
 sudo mv Powershell.sh /usr/local/bin/Powershell.sh
 #sudo chmod -R 777 /usr/local/bin/Powershell.sh
-cd  /usr/local/bin/ 
-Powershell.sh
+#cd  /usr/local/bin/ 
+#Powershell.sh
 
 #### INSTALL AZ Module###
-#wget https://raw.githubusercontent.com/mbouaklaine/MohamedBouaklaine/main/Powershell.sh
+wget https://raw.githubusercontent.com/mbouaklaine/MohamedBouaklaine/main/Powershell.sh
 
 
